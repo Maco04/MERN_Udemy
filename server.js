7082fa60f95d3e057ed69da7d3f91bae7c63e71e
maxpower04 <mmacojosh@gmail.com>
2022-05-29 00:56:34 +0800

    changing port

--- server.js
+++ server.js
@@ -31,4 +31,4 @@ if (process.env.NODE_ENV === 'production') {
                                                      
 // app.get('/', (req, res) => res.send('API Running'));
 const PORT=process.env.PORT || 5000;
-app. listen(PORT, () => console. log( 'Server started on port${PORT}'));
+app. listen(PORT, () => console. log( `Server started on port${PORT}`));
